0e7e7d09142c71d9997da3db406f0e148eec5543
Gelidus <gelidus@hotmail.sk>
2015-01-10 22:47:06 +0100

    [Tests] wall.h will now watch for any changes in bluefire

--- wall.sh
+++ wall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mocha --compilers coffee:coffee-script/register
+while true; do
+  change=$(inotifywait -r -e close_write,moved_to,create .)
+  change=${change#./ * }
+  mocha --compilers coffee:coffee-script/register
+done
