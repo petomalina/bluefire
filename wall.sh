69f36002c0ff069a0bf52825464c6522b4b9052e
Gelidus <gelidus@hotmail.sk>
2015-05-29 17:06:59 +0200

    [Wall] Added possibility to add arguments to mocha tester

--- wall.sh
+++ wall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-mocha --compilers coffee:coffee-script/register # first run
-
 while true; do
+  mocha $@ --compilers coffee:coffee-script/register
   change=$(inotifywait -r -e close_write,moved_to,create .)
   change=${change#./ * }
-  mocha --compilers coffee:coffee-script/register
 done
